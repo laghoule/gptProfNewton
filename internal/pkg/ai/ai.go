53c30ec4d881cc947f1cecd1c45d9457efe63e28
Pascal Gauthier <pgauthier@stingray.com>
2023-07-07 14:32:17 -0400

    Add moderation check & update dependency
    Signed-off-by: Pascal Gauthier <pgauthier@stingray.com>

--- internal/pkg/ai/ai.go
+++ internal/pkg/ai/ai.go
@@ -14,11 +14,11 @@ const (
 	Faisant usage du système métrique, tu communiques des concepts en utilisant un langage simple, des images mentales claires et des explications concrètes pour assurer une compréhension optimale de l'étudiant. 
 	Ton ton est constamment rempli d'enthousiasme, démontrant une passion palpable pour la transmission du savoir dans toutes ses dimensions. 
 	Même si tu te trouves dans un terminal texte, tu n'hésites pas à enrichir ton enseignement avec des références pertinentes sur le web, tout en restant dans le cadre du texte uniquement. 
-	Si jamais tu es confronté à une question dont la réponse échappe à ton champ de connaissances, tu diriges l'élève vers ses parents ou ses professeurs pour obtenir de l'aide supplémentaire. 
 	Dans le cas où un sujet pourrait ne pas convenir à un enfant en raison de sa nature sensible, tu le réfères à ses parents pour plus de conseils. 
 	
 	Directive clé: Ton rôle est d'assister et de guider ton étudiant dans son parcours d'apprentissage, sans jamais faire le travail à sa place.
 `
+	modErrorMsg = `Votre message a été signalé par le service de modération d'OpenAI. Pour garantir un environnement sûr et respectueux pour tous, nous vous demandons de revoir le contenu de vos messages.`
 )
 
 type AI struct {
@@ -46,9 +46,11 @@ func NewClient(conf Config) (*AI, error) {
 		return nil, err
 	}
 
-	var temperature float32
+	// https://community.openai.com/t/cheat-sheet-mastering-temperature-and-top-p-in-chatgpt-api-a-few-tips-and-tricks-on-controlling-the-creativity-deterministic-output-of-prompt-responses/172683
+	var temp, topP float32
 	if conf.Creative {
-		temperature = 0.7
+		temp = 0.5
+		topP = 0.5
 	}
 
 	prompt := fmt.Sprintf("%sDe plus, ajuste minutieusement tes réponses selon l'année scolaire de l'étudiant, dans le cas present l'année scolaire est %d. Plus le grade est élevé, plus la réponse est detailée", NewtonPrompt, conf.Grade)
@@ -57,7 +59,8 @@ func NewClient(conf Config) (*AI, error) {
 		client: openai.NewClient(key),
 		Request: &openai.ChatCompletionRequest{
 			Model:       model,
-			Temperature: temperature,
+			Temperature: temp,
+			TopP:        topP,
 			Messages: []openai.ChatCompletionMessage{
 				{
 					Role:    openai.ChatMessageRoleSystem,
@@ -71,11 +74,29 @@ func NewClient(conf Config) (*AI, error) {
 }
 
 func (a *AI) Chat(ctx context.Context) (openai.ChatCompletionResponse, error) {
-	return a.client.CreateChatCompletion(ctx, openai.ChatCompletionRequest(*a.Request))
+	ok, err := a.isChatSafe(ctx)
+	if err != nil {
+		return openai.ChatCompletionResponse{}, err
+	}
+
+	if ok {
+		return a.client.CreateChatCompletion(ctx, openai.ChatCompletionRequest(*a.Request))
+	}
+
+	return openai.ChatCompletionResponse{}, fmt.Errorf(modErrorMsg)
 }
 
 func (a *AI) ChatStream(ctx context.Context) (*openai.ChatCompletionStream, error) {
-	return a.client.CreateChatCompletionStream(ctx, openai.ChatCompletionRequest(*a.Request))
+	ok, err := a.isChatSafe(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	if ok {
+		return a.client.CreateChatCompletionStream(ctx, openai.ChatCompletionRequest(*a.Request))
+	}
+
+	return nil, fmt.Errorf(modErrorMsg)
 }
 
 func (a *AI) Reset() {
@@ -86,12 +107,29 @@ func (a *AI) CancelLastMessage() {
 	a.Request.Messages = a.Request.Messages[:len(a.Request.Messages)-1]
 }
 
+func (a *AI) isChatSafe(ctx context.Context) (bool, error) {
+	modRes, err := a.client.Moderations(ctx, openai.ModerationRequest{
+		Input: a.Request.Messages[len(a.Request.Messages)-1].Content,
+	})
+	if err != nil {
+		return false, err
+	}
+
+	for _, result := range modRes.Results {
+		if result.Flagged {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
+
 func getModel(m string) (string, error) {
 	var models = []string{"gpt-3.5", "gpt-4"}
 
 	switch m {
 	case "gpt-3.5":
-		return openai.GPT3Dot5Turbo16K0613, nil
+		return openai.GPT3Dot5Turbo16K, nil
 	case "gpt-4":
 		return openai.GPT4, nil
 	default:
