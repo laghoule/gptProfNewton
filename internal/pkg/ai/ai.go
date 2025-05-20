72dcc2b7f78c7b2807d2dd65228d979c0b63ec6e
Pascal Gauthier <pgauthier@nihilisme.ca>
2025-05-19 21:09:12 -0400

    Update prompt + detect thinking model
    Signed-off-by: Pascal Gauthier <pgauthier@nihilisme.ca>

--- internal/pkg/ai/ai.go
+++ internal/pkg/ai/ai.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"regexp"
 
 	"github.com/laghoule/gptProfNewton/internal/pkg/config"
 
@@ -15,9 +16,8 @@ const (
 	NewtonPrompt = `Tu es connu sous le nom de Professeur Newton. 
 	Ton rôle consiste à agir comme un tuteur et un guide éducatif pour des élèves.
 	Si le nom de l'étudiant est connu, tu peux l'utiliser pour creer un lien de confiance.
-	Faisant usage du système métrique, tu communiques des concepts en utilisant un langage simple, des images mentales claires et des explications concrètes. 
+	Faisant usage du système métrique, tu communiques des concepts en utilisant un langage adapté, des imageries claires et des explications concrètes. 
 	Utilise un ton enthousiaste, démontrant une passion palpable pour la transmission du savoir dans toutes ses dimensions. 
-	Tu n'hésites pas à enrichir ton enseignement avec des références pertinentes sur le web (page web et video youtube) 
 	Dans le cas où un sujet pourrait ne pas convenir à un enfant en raison de sa nature sensible, tu le réfères à ses parents pour plus de conseils.
 	
 	Directive clé: Ton rôle est d'assister et de guider ton étudiant dans son parcours d'apprentissage, sans jamais faire le travail à sa place.
@@ -33,7 +33,6 @@ type AI struct {
 }
 
 func NewClient(conf *config.Config, debug bool) (*AI, error) {
-	// TODO: do better than this
 	key, found := os.LookupEnv("OPENAI_API_KEY")
 	if !found && conf.OpenAI.APIKey == "" {
 		return nil, missingKeyErr()
@@ -55,12 +54,12 @@ func NewClient(conf *config.Config, debug bool) (*AI, error) {
 		temp = 0.2
 	}
 
-	// o3 model has beta-limitations, temperature, top_p and n are fixed at 1, while presence_penalty and frequency_penalty are fixed at 0
-	if conf.OpenAI.Model == "o3-mini" {
+	// thinking model have limitations, temperature, top_p and n are fixed at 1, while presence_penalty and frequency_penalty are fixed at 0
+	if isThinkingModel(conf.OpenAI.Model) {
 		temp = 1
 	}
 
-	prompt := fmt.Sprintf("%s\nDe plus, ajuste minutieusement tes réponses selon l'année scolaire de l'étudiant, dans le cas present l'année scolaire est %d. Plus le grade est élevé, plus la réponse est detailée", NewtonPrompt, conf.Student.Grade)
+	prompt := fmt.Sprintf("%s\nAjuste tes réponses selon l'année scolaire de l'étudiant, dans le cas present l'année scolaire est %d. Plus le grade est élevé, plus la réponse est detailée", NewtonPrompt, conf.Student.Grade)
 	prompt = fmt.Sprintf("%s\nLe prénom de ton étudiant est %s.", prompt, conf.Student.Name)
 	prompt = fmt.Sprintf("%s\nLes particularitées de l'étudiant sont:\n%s.", prompt, conf.Student.Details)
 
@@ -137,4 +136,9 @@ func getModel(client *openai.Client, ctx context.Context, m string) (string, err
 	}
 
 	return "", invalidModelErr()
+}
+
+func isThinkingModel(model string) bool {
+	r := regexp.MustCompile(`^o[1-9]-.*`)
+	return r.MatchString(model)
 }
